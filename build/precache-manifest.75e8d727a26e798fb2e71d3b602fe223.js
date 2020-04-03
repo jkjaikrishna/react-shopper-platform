372472ccd16a8559981b30b34d86f51bcdc6c908
jkjaikrishna <jayakrishnan.tn@velosit.in>
2020-04-03 10:06:30 +0530

    build added

---
+++ build/precache-manifest.75e8d727a26e798fb2e71d3b602fe223.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "433d2e5d135da219896f6d8963de0e15",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68dc1c15df021152fefb",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "48c3bb86b055a8a969bf",
+    "url": "/static/css/main.e94e8771.chunk.css"
+  },
+  {
+    "revision": "68dc1c15df021152fefb",
+    "url": "/static/js/2.e7a8ad35.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e7a8ad35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48c3bb86b055a8a969bf",
+    "url": "/static/js/main.09485c24.chunk.js"
+  },
+  {
+    "revision": "b7bb12324c5333a8f28b",
+    "url": "/static/js/runtime-main.9dbe4080.js"
+  }
+]);
